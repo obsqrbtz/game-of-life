fe5780398d880cdf646b94a209b993e1360fdc95
Daniel Dada <doesdeos@gmail.com>
2017-08-01 19:50:38 +0300

    Minor changes

--- grid.cpp
+++ grid.cpp
@@ -4,6 +4,9 @@ namespace game {
     grid::grid(int iNew, int jNew){
         cell.resize(iNew + 1);
         for(int i = 0; i <= iNew; i++) cell[i].resize(jNew + 1);
+        for (int i = 0; i <= iNew; i++){
+            for (int j = 0; j <= jNew; j++) cell[i][j] = false;
+        }
         iMax = iNew;
         jMax = jNew;
     }
@@ -16,7 +19,7 @@ namespace game {
 
     grid grid::calculateNewState(grid tGrid){
         grid nGrid = tGrid;
-        for (int i = 1; i < iMax; i++){
+       for (int i = 1; i < iMax; i++){
             for(int j = 1; j < jMax; j++){
                 int neighbors = 0;
                 for(int in = i - 1; in <= i + 1; in++){
@@ -30,6 +33,7 @@ namespace game {
                 if (!tGrid.cell[i][j] && neighbors == 3) nGrid.cell[i][j] = true;
             }
         }
+
     return nGrid;
     }
     void grid::writeStateToFile(std::string filename){
@@ -46,8 +50,8 @@ namespace game {
         for(int i = 0; i <= iMax; i++){
             for(int j = 0; j <= jMax; j++){
                 input >> temp;
-                if(temp == '0') cell[i][j] = false;
-                if(temp == '1') cell[i][j] = true;
+                if(temp == '0' || i == 0 || i == iMax || j == 0 || j == jMax) cell[i][j] = false;
+                if(temp == '1' && i != 0 && i != iMax && j != 0 && j != jMax) cell[i][j] = true;
             }
         }
         input.close();
